b29b03f624364e87a0b7240eb29d834c624a25f1
hanifahaksiomatika <128040561+hanifahaksiomatika@users.noreply.github.com>
2024-02-06 05:37:47 +0700

    Add files via upload

---
+++ UAS_Inheritance/UAS_Inheritance/PenyejukRuangan.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAS_Inheritance
+{
+    public class PenyejukRuangan
+    {
+        // properties
+        public string Merek { get; set; }
+        public int Daya { get; set; }
+        
+        public PenyejukRuangan(string merek = "null", int daya = 0)     // class induk
+        {
+            Merek = merek;
+            Daya = daya;
+        }
+        public void GetMerekAndDaya()
+        {
+            Console.WriteLine("Merek Penyejuk Ruangan ini: {0} dengan Daya: {1}", Merek, Daya);
+        }
+    }
+    public class AC : PenyejukRuangan               // class anak
+    {
+        public int Suhu { get; set; }
+
+        // constructor
+        public AC (string merek = "null", int daya = 0, int suhu = 1)
+        {
+            Merek = merek;
+            Daya = daya;
+            Suhu = suhu;
+        }
+    }
+    public class KipasAngin : PenyejukRuangan       // class anak
+    {
+        public int Kecepatan { get; set; }
+
+        // constructor
+        public KipasAngin(string merek = "null", int daya = 0, int kecepatan = 1)
+        {
+            Merek = merek;
+            Daya = daya;
+            Kecepatan = kecepatan;
+        }
+    }
+}
---
+++ UAS_Inheritance/UAS_Inheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAS_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PenyejukRuangan penyejukRuangan = new PenyejukRuangan("Panasonic", 200);
+            penyejukRuangan.GetMerekAndDaya();
+
+            AC ac = new AC("Sharp", 1000, 24);
+            ac.GetMerekAndDaya();
+
+            KipasAngin kipasAngin = new KipasAngin("National", 300, 3);
+            kipasAngin.GetMerekAndDaya();
+
+            /*PenyejukRuangan penyejukRuangan = new PenyejukRuangan();    //mengakses property
+             penyejukRuangan.Merek = "Panasonic";
+             penyejukRuangan.Daya = 200;
+             penyejukRuangan.GetMerekAndDaya();
+
+             AC ac = new AC();
+             ac.Merek = "Sharp";
+             ac.Daya = 1000;
+             ac.Suhu = 24;
+             ac.GetMerekAndDaya();
+
+             KipasAngin kipasAngin = new KipasAngin();
+             kipasAngin.Merek = "National";
+             kipasAngin.Daya = 300;
+             kipasAngin.Kecepatan = 3;
+             kipasAngin.GetMerekAndDaya();
+             */
+
+
+            Console.ReadKey();
+        }
+    }
+}
